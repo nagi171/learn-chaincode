ddf7c805ab950afa8b4825ca8d1b783029b7f3a6
IIC <IIC>
2017-03-30 14:35:25 +0200

    XX

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -57,6 +57,8 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 		return t.Init(stub, "init", args)
 	} else if function == "write" {
 		return t.write(stub, args)
+	} else if function == "changeowner" {
+		return t.changeOwner(stub, args)
 	}
 	fmt.Println("invoke did not find func: " + function)
 
